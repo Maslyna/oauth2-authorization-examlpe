dee88e05407730c6ca6aa9284f51c135796af3c6
Maslyna <sanci324@gmail.com>
2023-12-04 13:00:30 +0100

    added: auth-server

---
+++ oauth2-authorization-server/src/main/java/net/maslyna/authorization/server/AuthServerApp.java
@@ -0,0 +1,11 @@
+package net.maslyna.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApp.class, args);
+    }
+}
