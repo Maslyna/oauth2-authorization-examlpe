f5c6dad7cbaf579615f95fa873278277be506711
Maslyna <sanci324@gmail.com>
2023-12-09 14:14:31 +0100

    added: oauth2-resource-server

---
+++ oauth2-resource-server/src/main/java/net/maslyna/resource/server/ResourceServerApp.java
@@ -0,0 +1,11 @@
+package net.maslyna.resource.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApp.class, args);
+    }
+}
